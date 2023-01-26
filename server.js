43c3bf1559cc93392869ad4a7121be322a43a445
André Godeiro <andrevictor.godeiro@gmail.com>
2023-01-26 15:40:45 -0300

    feat: server and router files created

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = require("./routers");
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3000, () => console.log("Server running on port 3000"));
